9a78e4708446a451908910c120de7ac9be509069
govindraj-snippet <2023bit035@sggs.ac.in>
2026-03-18 00:27:13 +0530

    initial commit

---
+++ src/main/java/com/govi/shadowDBA/ShadowDbaApplication.java
@@ -0,0 +1,13 @@
+package com.govi.shadowDBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowDbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowDbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/govi/shadowDBA/ShadowDbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govi.shadowDBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowDbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
